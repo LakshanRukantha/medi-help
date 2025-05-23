b5cebc89dc5bb58cf4e85281355ad98f3c5d595e
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2025-05-23 11:42:08 +0530

    feat: initialize shadcn ui

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,16 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
   return (
-    <div className="text-center text-white pb-2 bg-blue-500 text-5xl">
-      MediHelp
-    </div>
+    <>
+      <div className="text-center text-white pb-2 bg-blue-500 text-5xl">
+        <h1>MediHelp</h1>
+      </div>
+      <Button className="mt-4" variant="default">
+        Get Started
+      </Button>
+    </>
   );
 };
 
